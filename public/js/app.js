711863c2ba52ed938cd59ff67c767e85d5ed4e11
Your Name <you@example.com>
2020-01-14 18:50:34 -0600

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+
+const weatherForm = document.querySelector('form')
+const messageOne = document.querySelector('#error')
+const messageTwo = document.querySelector('#message')
+
+weatherForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const location = e.target.elements.search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response) =>{
+        response.json().then((data)=>{
+            if(data.error){
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
+            }else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/970b46ac94dc08d324f7de1177f1cb0f/' + latitude +','+ longitude +'?units=si'
+
+    request({url, json:true}, (error, response, body) =>{
+    
+        if(error){
+           callback(error)    
+        }else if(body.error){
+            callback(error)   
+        }
+        else{
+           const data = `${body.daily.data[0].summary}. It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
+           callback(undefined, data)
+        }
+    })
+}
+
+module.exports = forecast
